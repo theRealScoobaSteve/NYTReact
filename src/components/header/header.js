022ade1870be2bdea508a404ff7f7639281c4f92
theRealScoobaSteve <stephensimone@live.com>
2017-10-31 16:44:52 -0700

    Created 3 components: header, input-group, search-form and got them to render to the app

---
+++ src/components/header/header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+const HEADER = () =>
+<div className="jumbotron">
+    <h1 className="display-3">React NYT Article Search</h1>
+    <p className="lead">Search for a news article below and save interesting ones</p>
+    <hr className="my-4" />
+</div> 
+
+
+export default HEADER
---
+++ src/components/search-form/search-form.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import INPUT_GROUP from "../input-group"
+
+const SEARCH_FORM = () =>
+<INPUT_GROUP type="Username" />
+
+
+export default SEARCH_FORM
