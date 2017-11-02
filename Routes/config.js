efb7a74d660f3e96290e6411324fc13ac3fb25fd
theRealScoobaSteve <stephensimone@live.com>
2017-11-02 16:59:18 -0700

    got the title to post and got a response from the api

---
+++ Routes/config.js
@@ -0,0 +1,5 @@
+var config = {
+    MY_KEY : "00529eb97bc9429dad7754c4e0e8409c"
+}
+
+module.exports = config
--- Routes/routes.js
+++ Routes/routes.js
@@ -1,9 +1,12 @@
 // Makes HTTP request for HTML page
-var REQUEST = require("request")
+const REQUEST = require("request")
+const AXIOS = require("axios")
 const PATH = require('path')
 const MONGO = require("../ORM/mongoObject.js")
 const BODYPARSER = require("body-parser")
 let mongo = new MONGO("mongodb://Steve:123456@ds149974.mlab.com:49974/mongo-scraper")
+const API_KEY = require("./config.js")
+const EXPRESS = require('express')
 
 module.exports = (APP) =>
 {
@@ -35,4 +38,26 @@ module.exports = (APP) =>
         response.sendFile(PATH.resolve(__dirname, '../react-ui/build', 'index.html'))
     })
 
+    APP.post("/api/search", function(req, res)
+    {
+        const POST_REQUEST = req.body;
+
+        console.log(POST_REQUEST)
+
+        REQUEST.get(
+        {
+            url: "https://api.nytimes.com/svc/search/v2/articlesearch.json",
+            qs: 
+            {
+              'api-key': API_KEY.MY_KEY,
+              'q': POST_REQUEST.title
+            },
+        }, function(err, response, body) 
+        {
+            body = JSON.parse(body);
+            console.log(body);
+        })
+
+         
+    })
 }
