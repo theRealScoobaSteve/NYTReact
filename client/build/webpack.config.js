f1a7988249bb27c30b00d53fe7477918ac7d3cd2
theRealScoobaSteve <stephensimone@live.com>
2019-09-01 18:59:12 -0700

    Add webpack plugin and refactor webpack.config

--- client/build/webpack.config.js
+++ client/build/webpack.config.js
@@ -1,13 +1,24 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-  // 1
   entry: "./src/index.js",
-  // 2
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["*", ".js"]
+  },
+  plugins: [new HtmlWebpackPlugin()],
   output: {
     path: __dirname + "/dist",
     publicPath: "/",
     filename: "bundle.js"
   },
-  // 3
   devServer: {
     contentBase: "./dist"
   }
