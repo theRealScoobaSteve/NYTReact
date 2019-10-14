990caba2cf2b60f665233a60bc200d782245a9c4
theRealScoobaSteve <stephensimone@live.com>
2019-10-13 20:01:05 -0700

    Add CRUD for articles

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,73 +1,130 @@
-import { Controller, Post, Body } from "@nestjs/common";
-import { AppService } from "./app.service";
-import { HttpResponse } from "./http.response";
+import { Controller, Post, Body, Delete, Param, Get } from "@nestjs/common";
+import { ApiModelProperty } from "@nestjs/swagger";
+import { EntityManager } from "typeorm";
 
 import { nytAPI } from "./util/nyt.api";
+import { HttpResponse } from "./http.response";
+import { Image } from "./database/entities/image.entity";
+import { Author } from "./database/entities/author.entity";
+import { Article } from "./database/entities/article.entity";
 
 require("dotenv").config();
 
 const apiURL = `articlesearch.json?api-key=${process.env.NYT_KEY}&q=`;
 
 export class FetchArticleDTO {
+  @ApiModelProperty()
   readonly query: string;
 }
 
 export class SaveArticleDTO {
+  @ApiModelProperty()
   readonly article: ArticleDTO;
+  @ApiModelProperty()
   readonly images: ImageArticle[];
+  @ApiModelProperty()
   readonly authors: AuthorArticle[];
 }
 
 export class ArticleDTO {
+  @ApiModelProperty()
   readonly webUrl: string;
+  @ApiModelProperty()
   readonly snippet: string;
+  @ApiModelProperty()
   readonly headline: string;
+  @ApiModelProperty()
   readonly pubDate: string;
 }
 
 export class ImageArticle {
+  @ApiModelProperty()
   readonly height: number;
+  @ApiModelProperty()
   readonly width: number;
+  @ApiModelProperty()
   readonly caption: string;
+  @ApiModelProperty()
   readonly url: string;
 }
 
 export class AuthorArticle {
+  @ApiModelProperty()
   readonly firstName: string;
+  @ApiModelProperty()
   readonly lastName: string;
+  @ApiModelProperty()
   readonly role: string;
+  @ApiModelProperty()
   readonly organization: string;
+  @ApiModelProperty()
   readonly title: string;
 }
 
 // tslint:disable-next-line: max-classes-per-file
 @Controller("api")
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly entityManager: EntityManager) {}
 
   @Post("articles")
-  fetchArticles(
+  async fetchArticles(
     @Body() fetchArticleDTO: FetchArticleDTO,
   ): Promise<HttpResponse> {
+    const { query } = fetchArticleDTO;
+    const data = await nytAPI.get(`${apiURL}${query}`);
+
+    return new HttpResponse(true, "", data);
+  }
+
+  @Post("favorites")
+  async saveArticle(
+    @Body() saveArticleDTO: SaveArticleDTO,
+  ): Promise<HttpResponse> {
     try {
-      const { query } = fetchArticleDTO;
-      return nytAPI
-        .get(`${apiURL}${query}`)
-        .then(({ data }) => new HttpResponse(true, "", data));
+      const { article, images, authors } = saveArticleDTO;
+      let imagesToSave = [];
+      let authorsToSave = [];
+
+      images.forEach(image => {
+        imagesToSave.push(this.entityManager.create(Image, image));
+      });
+
+      authors.forEach(author => {
+        authorsToSave.push(this.entityManager.create(Author, authors));
+      });
+
+      const articleToSave = this.entityManager.create(Article, article);
+
+      articleToSave.authors = authorsToSave;
+      articleToSave.images = imagesToSave;
+
+      return new HttpResponse(true, "Article Successfully Saved");
     } catch (e) {
-      console.error(e);
+      return new HttpResponse(false, e);
     }
   }
 
-  @Post("favorite")
-  saveArticle(@Body() saveArticleDTO: SaveArticleDTO): Promise<HttpResponse> {
-    try {
-      const { article, images, authors } = saveArticleDTO;
-      return nytAPI
-        .get(`${apiURL}${query}`)
-        .then(({ data }) => new HttpResponse(true, "", data));
-    } catch (e) {
-      console.error(e);
+  @Delete("favorites/:id")
+  async removeArticle(@Param() params): Promise<HttpResponse> {
+    const { id } = params;
+
+    const article = await this.entityManager.find(Article, id);
+
+    if (article) {
+      await this.entityManager.remove(article);
+
+      return new HttpResponse(true, "Article Successfully Removed");
     }
+
+    return new HttpResponse(false, "This Article Does not Exist");
+  }
+
+  @Get("favorites")
+  async fetchFavorites(): Promise<HttpResponse> {
+    const articles = await this.entityManager.find(Article, {
+      favorite: true,
+    });
+
+    return new HttpResponse(true, "Saved Articles", articles);
   }
 }
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,18 @@
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle("Block Ledger")
+    .setDescription("This is the documentation for the block ledger")
+    .setVersion("0.1.0")
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup("api", app, document);
+
   await app.listen(3001);
 }
 bootstrap();
